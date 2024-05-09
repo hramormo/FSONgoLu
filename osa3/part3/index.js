c5e99eeee516ab1db99a7b3c7d55439999e2098e
hramormo <sekinviela@hotmail.com>
2024-05-09 21:50:25 +0300

    Update index.js

--- osa3/part3/index.js
+++ osa3/part3/index.js
@@ -94,6 +94,6 @@ let notes = [
 app.use(unknownEndpoint)
 
 
-const PORT = 3001
-app.listen(PORT)
-console.log(`Server running on port ${PORT}`)
+const PORT = process.env.port || 3001
+app.listen(PORT, () => {
+console.log(`Server running on port ${PORT}`)})
